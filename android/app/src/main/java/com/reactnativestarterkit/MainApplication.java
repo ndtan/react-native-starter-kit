ea4ef111d54d335b8feb84bb922688fea8d5670a
Tan Nguyen <ndtanqb3k@gmail.com>
2022-10-20 16:35:44 +0700

    Fix bug: build failed on Android

--- android/app/src/main/java/com/reactnativestarterkit/MainApplication.java
+++ android/app/src/main/java/com/reactnativestarterkit/MainApplication.java
@@ -11,6 +11,8 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import com.microsoft.codepush.react.CodePush;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
